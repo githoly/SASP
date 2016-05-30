99359653c9eadcc8fbb82cb49767b5642b067426
Jason.Liu <Jason.Liu@software.dell.com>
2016-05-30 10:36:24 +0800

    add Autorun folder

---
+++ Autorun/tests/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright 2013 Dell Software Inc.
+# ALL RIGHTS RESERVED.
+#
+# This software is the confidential and proprietary information of
+# Dell Software Inc. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered
+# into with Dell Software Inc.
+#
+# DELL SOFTWARE INC. MAKES NO REPRESENTATIONS OR 
+# WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, 
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+# TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS 
+#  FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. DELL 
+# SOFTWARE SHALL NOT BE LIABLE FOR ANY DAMAGES 
+# SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING 
+# OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+#
